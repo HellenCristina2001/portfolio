8f4a4c88342db6dc754e0188c40daa7febbcfb31
HellenCristina <122121198+HellenCristina2001@users.noreply.github.com>
2023-03-28 18:20:49 -0300

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,12 @@
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+    
+console.log(validateEmail('texto@texto.com')); // true
+console.log(validateEmail('texto@texto')); // false
+console.log(validateEmail('texto.com')); // false
+console.log(validateEmail('texto')); // false
+
+
+
